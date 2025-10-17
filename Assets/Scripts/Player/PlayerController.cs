b77c2382f94d47f1d0e82b1908e1bbb619279cd8
joyce-yong <qingacc@gmail.com>
2025-10-17 20:18:02 +0800

    import fish model

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,9 @@ using UnityEngine;
         private Vector2 _frameVelocity;
         private bool _cachedQueryStartInColliders;
 
+        public ProjectileBehaviour ProjectilePrefab; //
+        public Transform LaunchOffset; //
+
         #region Interface
 
         public Vector2 FrameInput => _frameInput.Move;
@@ -34,6 +37,11 @@ using UnityEngine;
         {
             _time += Time.deltaTime;
             GatherInput();
+
+            if(FrameInput.GetButtonDown("Fire1")) //
+            {
+                Instantiate(ProjectilePrefab, LaunchOffset.position, transform.rotation);
+            }
         }
 
         private void GatherInput()
